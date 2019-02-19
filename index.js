37098a75963b15586395392ea8bde6c06a41101f
Zach Bloomquist <github@chary.us>
2019-02-19 11:58:11 -0500

    show listenin port

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const debug = require('debug')('proxy')
 
 const proxy = new httpProxy.createProxyServer()
 
+const port = process.env.PORT || 1337;
+
 const server = http.createServer((req, res) => {
     debug('Request for', req.url)
     proxy.web(req, res, { target: req.url })
-}).listen(process.env.PORT || '1337')
+}).listen(port, () => {
+    console.log(`HTTP proxy listening on http://localhost:${port}`)
+})
 
 server.addListener('connect', httpsProxy)
