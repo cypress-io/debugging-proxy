c180719af626bd26a5cb843e16cbce77bcef4336
Zach Bloomquist <github@chary.us>
2019-02-19 14:28:28 -0500

    Correct target

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const httpProxy = require('http-proxy')
 const httpsProxy = require('./https-proxy')
 const http = require('http')
+const url = require('url')
 const debug = require('debug')('proxy')
 
 const proxy = new httpProxy.createProxyServer()
@@ -8,8 +9,10 @@ const proxy = new httpProxy.createProxyServer()
 const port = process.env.PORT || 1337;
 
 const server = http.createServer((req, res) => {
-    debug('Request for', req.url)
-    proxy.web(req, res, { target: req.url })
+    const { host, protocol } = url.parse(req.url)
+    const target = `${protocol}//${host}`
+    debug('Request for', req.url, host, protocol)
+    proxy.web(req, res, { target })
 }).listen(port, () => {
     console.log(`HTTP proxy listening on http://localhost:${port}`)
 })
